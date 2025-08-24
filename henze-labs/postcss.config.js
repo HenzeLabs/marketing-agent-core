3df07e6d2ef8b3c6daf8f76805adb7fa57c67411
HenzeLabs <laurenhenze@gmail.com>
2025-08-24 17:05:53 -0400

    fix: Tailwind setup and CSS import

--- henze-labs/postcss.config.js
+++ henze-labs/postcss.config.js
@@ -4,3 +4,9 @@ import autoprefixer from 'autoprefixer'
 export default {
   plugins: [tailwind(), autoprefixer()],
 }
+import tailwind from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
+
+export default {
+  plugins: [tailwind(), autoprefixer()],
+}
